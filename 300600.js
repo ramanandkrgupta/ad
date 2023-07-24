2da845059a160d2672fc44f35ed4cb4b91d33912
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2023-07-25 04:37:36 +0530

    Create 300600.js

---
+++ 300600.js
@@ -0,0 +1,14 @@
+<script type='text/javascript'><!--//<![CDATA[
+   var m3_u = (location.protocol=='https:'?'https://nyxx.cf/www/delivery/ajs.php':'http://nyxx.cf/www/delivery/ajs.php');
+   var m3_r = Math.floor(Math.random()*99999999999);
+   if (!document.MAX_used) document.MAX_used = ',';
+   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+   document.write ("?zoneid=19");
+   document.write ('&amp;cb=' + m3_r);
+   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+   document.write ("&amp;loc=" + escape(window.location));
+   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+   if (document.context) document.write ("&context=" + escape(document.context));
+   document.write ("'><\/scr"+"ipt>");
+//]]>--></script><noscript><a href='https://nyxx.cf/www/delivery/ck.php?n=a46a7fc3&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://nyxx.cf/www/delivery/avw.php?zoneid=19&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a46a7fc3' border='0' alt='' /></a></noscript>
