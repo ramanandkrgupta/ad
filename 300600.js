c821dfb97ad8be5e2fcd35811ed1cb8b74065ae0
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2023-07-25 05:09:41 +0530

    Update 300600.js

--- 300600.js
+++ 300600.js
@@ -1,4 +1,4 @@
-<script type='text/javascript'><!--//<![CDATA[
+type='text/javascript'><!--//<![CDATA[
    var m3_u = (location.protocol=='https:'?'https://nyxx.cf/www/delivery/ajs.php':'http://nyxx.cf/www/delivery/ajs.php');
    var m3_r = Math.floor(Math.random()*99999999999);
    if (!document.MAX_used) document.MAX_used = ',';
@@ -11,4 +11,4 @@
    if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
    if (document.context) document.write ("&context=" + escape(document.context));
    document.write ("'><\/scr"+"ipt>");
-//]]>--></script><noscript><a href='https://nyxx.cf/www/delivery/ck.php?n=a46a7fc3&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://nyxx.cf/www/delivery/avw.php?zoneid=19&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a46a7fc3' border='0' alt='' /></a></noscript>
+//]]>-->
